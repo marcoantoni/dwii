9ec20725d054435953bc9b1cc2d429be9873a40e
marco <marco.antoni910@gmail.com>
2026-05-19 21:02:20 -0300

    atualização: inclusão da tabela municipios

--- des_web.sql
+++ des_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Tempo de geração: 15/04/2026 às 00:05
+-- Tempo de geração: 20/05/2026 às 00:01
 -- Versão do servidor: 8.3.0
 -- Versão do PHP: 8.3.30
 
@@ -38,17 +38,43 @@ CREATE TABLE `clientes` (
   `bb` int NOT NULL,
   `bradesco` int NOT NULL,
   `nubank` int NOT NULL,
-  `itau` int NOT NULL
+  `itau` int NOT NULL,
+  `id_municipio` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Despejando dados para a tabela `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `nome`, `nasc`, `fone`, `email`, `sexo`, `senha`, `bb`, `bradesco`, `nubank`, `itau`) VALUES
-(1, 'Mateus Augusto Barros', '2002-01-14', '55996457597', 'mateus.barros@ymail.com', 'm', '1234', 1, 0, 0, 0),
-(2, 'Mateus Augusto Barros', '2002-01-14', '55996457597', 'mateus.barros@ymail.com', 'm', '1234', 1, 0, 0, 0),
-(3, 'Camila da Rosa Santana', '2008-01-14', '519945457597', 'camila@ymail.com', 'f', '1234', 0, 0, 1, 0);
+INSERT INTO `clientes` (`id`, `nome`, `nasc`, `fone`, `email`, `sexo`, `senha`, `bb`, `bradesco`, `nubank`, `itau`, `id_municipio`) VALUES
+(1, 'Marguerite H. Rangel\n', '2002-01-14', '(51) 99847-2316', 'marguerite@rangel.eu', 'f', '1234', 1, 0, 0, 0, 1),
+(2, 'Mateus Augusto Barros', '2002-01-14', '(11) 98765-4021', 'mateus.barros@ymail.com', 'm', '1234', 1, 0, 0, 0, 2),
+(3, 'Camila da Rosa Santana', '2008-01-14', '519945457597', '(41) 99128-7743', 'f', '1234', 0, 0, 1, 0, 3),
+(14, 'Gabriel Augusto da Silva', '2001-01-15', '519945457597', '(85) 99631-5802', 'm', '1234', 1, 1, 1, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `municipios`
+--
+
+CREATE TABLE `municipios` (
+  `id` int NOT NULL,
+  `nome` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `municipios`
+--
+
+INSERT INTO `municipios` (`id`, `nome`) VALUES
+(1, 'Parobé'),
+(2, 'Rolante'),
+(3, 'Igrejinha'),
+(4, 'Taquara'),
+(5, 'Três Coroas'),
+(6, 'Riozinho'),
+(7, 'Santo Antônio da Patrulha');
 
 --
 -- Índices para tabelas despejadas
@@ -58,6 +84,13 @@ INSERT INTO `clientes` (`id`, `nome`, `nasc`, `fone`, `email`, `sexo`, `senha`, 
 -- Índices de tabela `clientes`
 --
 ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_clientes_municipios_idx` (`id_municipio`);
+
+--
+-- Índices de tabela `municipios`
+--
+ALTER TABLE `municipios`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -68,7 +101,23 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de tabela `municipios`
+--
+ALTER TABLE `municipios`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `fk_clientes_municipios` FOREIGN KEY (`id_municipio`) REFERENCES `municipios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
